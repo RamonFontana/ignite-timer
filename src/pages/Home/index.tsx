037072f4eb21540014465d8fdcb9d1dc8152dae8
Ramon Fontana <30450181+RamonFontana@users.noreply.github.com>
2023-11-09 17:17:03 -0300

    get better inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,13 +19,23 @@ export function Home() {
             id="task"
             type="text"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="nuber"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={5}
           />
 
           <span>minutos.</span>
